2cb16ad87a4b268059caa188ef5c476ba84abf6a
Aleksandr Sokolov <sokolov.aleksand427r@gmail.com>
2025-11-07 09:32:24 +0500

    Переделаны и добавлены unit тесты

---
+++ TaxiSimulation.Core.Tests/PositionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TaxiSimulation.Core.Models;
+
+namespace TaxiSimulation.Core.Tests
+{
+    [TestFixture]
+    public class PositionTests
+    {
+        [Test]
+        public void DistanceTo_ShouldReturnCorrectValue()
+        {
+            var a = new Position(0, 0);
+            var b = new Position(3, 4);
+
+            var distance = a.DistanceTo(b);
+            Assert.That(distance, Is.EqualTo(5.0).Within(1e-5));
+        }
+    }
+}
